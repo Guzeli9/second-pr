56bfd237c0169fef8996b4ab4002c04291eda510
Guzel <galiakbarovagyzelia@gmail.com>
2022-10-19 05:17:02 +0300

    initial

---
+++ ProjectForTesting/src/main/kotlin/main.kt
@@ -0,0 +1,29 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import kotlin.random.Random
+
+
+fun main() {
+    runBlocking {
+        launch {
+            GeneratorShared.sharedFlow.collect {
+                println(it)
+            }
+        }
+    }
+}
+
+object GeneratorShared {
+    private val scope = CoroutineScope(Job() + Dispatchers.Default)
+    private val _sharedFlow = MutableSharedFlow<Int>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    init {
+        scope.launch {
+            repeat(10) {
+                _sharedFlow.emit(Random.nextInt(90, 101))
+                delay(500)
+            }
+        }
+    }
+}
